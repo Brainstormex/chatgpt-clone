f3c9b12a4e1ceb214d4643e15fe1e5eae1099cc5
uzxr <uzersayed9@gmail.com>
2023-04-16 20:25:10 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Sidebar from './components/Sidebar'
 
 const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      hello
+      <Sidebar/>
     </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+
+
+const Sidebar = () => {
+  return (
+    <div className=" h-[100vh] flex w-[244px] flex-col justify-between bg-cyan-500">
+      <button className="flex justify-between bg-orange-600">+ New chat</button>
+      <ul></ul>
+      <nav className="flex justify-between">
+        <p className=" text-yellow-300">Made by uzxr</p>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
